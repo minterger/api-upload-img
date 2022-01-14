baf3a1cbcaeb99fceebae2e059990febfa51aa0f
minterger <minterger@gmail.com>
2022-01-14 19:34:56 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "public/uploads"));
+  },
+  filename: (req, file, cb) => {
+    const filename = file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    cb(null, filename);
+  }
+});
+const upload = multer({
+  storage,
+  dest: path.join(__dirname, "public/uploads"),
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+//get url
+const getUrl = (req) => {
+  const url = req.protocol + "://" + req.get("host");
+  return url;
+};
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  const url = getUrl(req);
+  const imageUrl = url + "/uploads/" + req.file.filename;
+  res.json({
+    url: imageUrl,
+  });
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+
+app.set('port', process.env.PORT || 4040);
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
